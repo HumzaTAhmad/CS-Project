cd5f8ed8e2b8667dae874b3607923cd632d13ed0
Aalayah Honablue <aah16@hood.edu>
2021-11-29 22:51:16 -0500

    Dates clickable & some commenting

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ const renderCalendar = () => {
 
   const monthDays = document.querySelector(".days");
 
+//Last day of current month
   const lastDay = new Date(
     date.getFullYear(),
     date.getMonth() + 1,
@@ -41,17 +42,18 @@ const renderCalendar = () => {
     "November",
     "December",
   ];
-
+//Displays the month
   document.querySelector(".date h1").innerHTML = months[date.getMonth()];
-
+//Displays the current date
   document.querySelector(".date p").innerHTML = new Date().toDateString();
 
   let days = "";
 
+  //Show the previous days from the last month
   for (let x = firstDayIndex; x > 0; x--) {
     days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
   }
-
+//Current date
   for (let i = 1; i <= lastDay; i++) {
     if (
       i === new Date().getDate() &&
@@ -62,21 +64,24 @@ const renderCalendar = () => {
       days += `<div>${i}</div>`;
     }
   }
-
+//Show the days from the next month
   for (let j = 1; j <= nextDays; j++) {
     days += `<div class="next-date">${j}</div>`;
     monthDays.innerHTML = days;
   }
 };
 
+//Get the previous month when clicked
 document.querySelector(".prev").addEventListener("click", () => {
   date.setMonth(date.getMonth() - 1);
   renderCalendar();
 });
 
+//Get the next month when clicked
 document.querySelector(".next").addEventListener("click", () => {
   date.setMonth(date.getMonth() + 1);
   renderCalendar();
 });
+
 
 renderCalendar();
